a8f7b6f41424d275b4034227bba62c096574b0b3
ldody <leo.dody01@gmail.com>
2025-11-18 16:50:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import sys
 from pathlib import Path
+import torch
+import os
+
+torch.set_num_threads(1000)          # PyTorch internal compute threads
+torch.set_num_interop_threads(1000)  # Cross-op parallelism
+
+os.environ["OMP_NUM_THREADS"] = "1000"
+os.environ["MKL_NUM_THREADS"] = "1000"
+os.environ["NUMEXPR_NUM_THREADS"] = "1000"
 
 # Ensure src/ is on the Python path
 base_dir = Path(__file__).resolve().parent
 src_dir = base_dir / "src"
 if str(src_dir) not in sys.path:
-    sys.path.insert(0, str(src_dir))
+	sys.path.insert(0, str(src_dir))
 
 from drl_portfolio.training.train_sac import run_full_experiment
 
 
 if __name__ == "__main__":
-    data_dir = base_dir / "data"
-    results_dir = base_dir / "results"
-    #results_dir.mkdir(exist_ok=True)
+	data_dir = base_dir / "data"
+	results_dir = base_dir / "results"
+	#results_dir.mkdir(exist_ok=True)
 
-    run_full_experiment(
-        data_dir=data_dir,
-        results_dir=results_dir,
-        window=20,          # 20 previous timestamps
-        num_episodes=50,    # increase for real training
-    )
+	run_full_experiment(
+		data_dir=data_dir,
+		results_dir=results_dir,
+		window=20,          # 20 previous timestamps
+		num_episodes=50,    # increase for real training
+	)
