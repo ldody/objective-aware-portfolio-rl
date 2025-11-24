b97c47fba612916d0137802415df90f64a7cf43c
ldody <leo.dody01@gmail.com>
2025-11-24 17:06:27 +0100

    Update deploy_venv.bash

--- deploy_venv.bash
+++ deploy_venv.bash
@@ -11,8 +11,8 @@
 
 module purge
 module use /easybuild/AlmaLinux/8/skylake-avx512/mlxln5.5/foss2022b/modules/all
-module load Python/3.10.4-GCCcore-12.2.0
-source /home_nfs/polytech/leo.dody/PhD/Article_2/PhD_article_2/.venv/bin/activate
+module load Python/3.10.8-GCCcore-12.2.0
+source /home_nfs/polytech/leo.dody/PhD/Article_3/PhD_article_3/.venv/bin/activate
 
 python3 -m pip install -r requirements.txt
 
