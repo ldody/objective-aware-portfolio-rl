ea7932f0f008b3ff134dadfedc34c5e703955e4c
ldody <leo.dody01@gmail.com>
2025-12-22 15:43:58 +0100

    Update train_sac.py

--- src/drl_portfolio/training/train_sac.py
+++ src/drl_portfolio/training/train_sac.py
@@ -313,9 +313,9 @@ def run_full_experiment(
 		print("\nTraining SAC (constraint-aware)...")
 		sac_agent = train_sac_on_vec_env(train_vec_env, cfg)
 		bt_sac = backtest_policy(test_env, sac_agent, deterministic=True)
-        # DRL constrained
-        bt_sac["allocation"].to_csv(results_dir / f"alloc_sac_constrained_{'_'.join(filename_ext)}.csv")
-        bt_sac["performance"].to_csv(results_dir / f"perf_sac_constrained_{'_'.join(filename_ext)}.csv")
+		# DRL constrained
+		bt_sac["allocation"].to_csv(results_dir / f"alloc_sac_constrained_{'_'.join(filename_ext)}.csv")
+		bt_sac["performance"].to_csv(results_dir / f"perf_sac_constrained_{'_'.join(filename_ext)}.csv")
 		# sac_agent = train_sac_on_env(train_env, cfg)
 
 	if results_dir / f"alloc_sac_baseline_{'_'.join(filename_ext)}.csv" in os.listdir(results_dir):
@@ -325,7 +325,7 @@ def run_full_experiment(
 		print("\nTraining SAC baseline (no constraints)...")
 		sac_agent_base = train_sac_on_vec_env(train_vec_env_nc, cfg)
 		bt_sac_base = backtest_policy(test_env_nc, sac_agent_base, deterministic=True)
-        # DRL baseline
+		# DRL baseline
 		bt_sac_base["allocation"].to_csv(results_dir / f"alloc_sac_baseline_{'_'.join(filename_ext)}.csv")
 		bt_sac_base["performance"].to_csv(results_dir / f"perf_sac_baseline_{'_'.join(filename_ext)}.csv")
 		# sac_agent_base = train_sac_on_env(train_env_nc, cfg)
