0dcafed2677e1764fc46939e0a5aacde92466842
ldody <leo.dody01@gmail.com>
2026-03-10 17:20:50 +0100

    Update train_sac.py

--- src/drl_portfolio/training/train_sac.py
+++ src/drl_portfolio/training/train_sac.py
@@ -305,18 +305,18 @@ def run_full_experiment(
 	)
 
 	filename_ext = [str(v) for v in kwargs.values()]
-
+	'''
 	if (results_dir / f"alloc_sac_constrained_{'_'.join(filename_ext)}.csv").exists():
 		print("\nTraining SAC (constraint-aware) already done")
 		
-	else:
-		print("\nTraining SAC (constraint-aware)...")
-		sac_agent = train_sac_on_vec_env(train_vec_env, cfg)
-		bt_sac = backtest_policy(test_env, sac_agent, deterministic=True)
-		# DRL constrained
-		bt_sac["allocation"].to_csv(results_dir / f"alloc_sac_constrained_{'_'.join(filename_ext)}.csv")
-		bt_sac["performance"].to_csv(results_dir / f"perf_sac_constrained_{'_'.join(filename_ext)}.csv")
-		# sac_agent = train_sac_on_env(train_env, cfg)
+	else:'''
+	print("\nTraining SAC (constraint-aware)...")
+	sac_agent = train_sac_on_vec_env(train_vec_env, cfg)
+	bt_sac = backtest_policy(test_env, sac_agent, deterministic=True)
+	# DRL constrained
+	bt_sac["allocation"].to_csv(results_dir / f"alloc_sac_constrained_{'_'.join(filename_ext)}.csv")
+	bt_sac["performance"].to_csv(results_dir / f"perf_sac_constrained_{'_'.join(filename_ext)}.csv")
+	# sac_agent = train_sac_on_env(train_env, cfg)
 
 	if (results_dir / f"alloc_sac_baseline_{'_'.join(filename_ext)}.csv").exists():
 		print("\nTraining SAC baseline (no constraints) already done")
